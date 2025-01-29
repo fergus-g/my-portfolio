1ff113b36aedc89d4597610db4c8ad1c10059bb8
fergus-g <fergus.gildea@utrack.com>
2025-01-29 20:19:16 +0000

    Updated styling and added carousel

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
